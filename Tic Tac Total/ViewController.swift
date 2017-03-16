a8b56241b57c8bd94b787e8bea906049e5814937
Andrew Bellamy <andymanbellamy@gmail.com>
2017-03-16 20:10:48 +1030

    initial commit

---
+++ Tic Tac Total/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tic Tac Total
+//
+//  Created by Andrew Bellamy on 16/3/17.
+//  Copyright © 2017 Andrew Bellamy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
