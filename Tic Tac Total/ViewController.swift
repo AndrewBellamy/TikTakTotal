173bb7fb2d5d583784f753c757ff35bb850fda19
Andrew Bellamy <andymanbellamy@gmail.com>
2017-03-19 15:52:41 +1030

    Build issues

--- Tic Tac Total/ViewController.swift
+++ Tic Tac Total/ViewController.swift
@@ -12,12 +12,13 @@ class ViewController: UIViewController {
     
     let nought:Int = 3
     let cross:Int = 4
+    var finished:Bool = false
     
     var gameHistory = [Game]()
     
     var playerOne = Player(name: "player one", avatar: "2", symbol: "1", point: 3, score: 0),
         playerTwo = Player(name: "player two", avatar: "19", symbol: "4", point: 4, score: 0),
-        gameBoard = Board(A: 0,B: 0,C: 0,D: 0,E: 0,F: 0,G: 0,H: 0,I: 0),
+        gameBoard:Board?,
         currentPlayer:Player?,
         restingPlayer:Player?
     
@@ -33,6 +34,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     @IBAction func newGame(_ sender: Any) {
+        gameBoard = Board(A: 0,B: 0,C: 0,D: 0,E: 0,F: 0,G: 0,H: 0,I: 0)
         currentPlayer = playerOne
         restingPlayer = playerTwo
         updatePlayerLabel()
@@ -40,22 +42,47 @@ class ViewController: UIViewController {
 
     @IBAction func squareA(_ sender: UIButton) {
         sender.setImage(UIImage(named: (currentPlayer?.symbol)!), for: .normal)
-        gameBoard.A = (currentPlayer?.point)!
+        gameBoard?.A = (currentPlayer?.point)!
+        changePlayer()
+        sender.isEnabled = false
+    }
+    
+    @IBAction func squareB(_ sender: UIButton) {
+        sender.setImage(UIImage(named: (currentPlayer?.symbol)!), for: .normal)
+        gameBoard?.B = (currentPlayer?.point)!
+        changePlayer()
+        sender.isEnabled = false
+    }
+    
+    @IBAction func squareC(_ sender: UIButton) {
+        sender.setImage(UIImage(named: (currentPlayer?.symbol)!), for: .normal)
+        gameBoard?.C = (currentPlayer?.point)!
+        changePlayer()
+        sender.isEnabled = false
+    }
+    
+    @IBAction func squareD(_ sender: UIButton) {
+        sender.setImage(UIImage(named: (currentPlayer?.symbol)!), for: .normal)
+        gameBoard?.D = (currentPlayer?.point)!
         changePlayer()
         sender.isEnabled = false
     }
     
     func changePlayer() {
-        let temp = currentPlayer
-        currentPlayer = restingPlayer
-        restingPlayer = temp
+        let tempCurrent = currentPlayer
+        let tempResting = restingPlayer
+        currentPlayer = tempResting
+        restingPlayer = tempCurrent
         updatePlayerLabel()
     }
     
     func updatePlayerLabel() {
         runTime.text = currentPlayer?.name
     }
+    
+    
 }
+
 struct Board {
     var A:Int,
     B:Int,
@@ -70,12 +97,7 @@ struct Board {
 
 struct Game {
     var gameBoard:Board
-    
-    var currentPlayer:Player
-
-    mutating func changePlayer(player: Player) {
-        currentPlayer = player
-    }
+    var winner:Player
 }
 
 struct Player {
